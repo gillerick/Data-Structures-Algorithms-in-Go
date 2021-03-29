90b746613a784d787706c6e5f2900c9d865b4758
gillerick <=>
2021-03-29 15:27:28 +0300

    Added AddElement function

--- Sets/main.go
+++ Sets/main.go
@@ -8,3 +8,11 @@ type Set struct {
 func (set *Set) New() {
 	set.integerMap = make(map[int]bool)
 }
+
+//The AddElement function adds the element to integerMap if the element is not in the set
+func (set *Set) AddElement(element int)  {
+	if !set.ContainsElement(element){
+		set.integerMap[element] = true
+	}
+
+}
